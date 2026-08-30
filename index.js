cae6fab3f23657d633125c4d280459c115d98c17
Tyrone <tyronemrenje1985@gmail.com>
2026-08-30 20:10:49 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Learning DevOps is fun!');
+console.log('Starting the DevOps pipeline...');
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('starting tests...');
+
+setTimeout(()=>console.log('Almost there...'), 3000);
+
+console.log('Tests completed!');
